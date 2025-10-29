f1c332994184f6046622213817c2fb4f754a9ae5
UberGit3001 <mehdi.sebillot@laplateforme.io>
2025-10-29 10:30:11 +0100

    afficher les jours de semaine dans la console

---
+++ Jour01/job05/script05.js
@@ -0,0 +1,12 @@
+function afficherjourssemaines() {
+    // Tableau des jours de la semaine
+    const jourssemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+    // Boucle for pour afficher chaque jour
+    for (let i = 0; i < jourssemaines.length; i++) {
+        console.log(jourssemaines[i]);
+    }
+}
+
+// Appel de la fonction pour afficher dans la console
+afficherjourssemaines();

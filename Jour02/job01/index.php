9e61a70229df9d2d156bce3d630bf1f4d37850d0
UberGit3001 <mehdi.sebillot@laplateforme.io>
2025-10-31 00:16:28 +0100

    afficher la citation dans la console a chaque appuis du bouton

---
+++ Jour02/job01/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour02 - job01</title>
+</head>
+<body>
+    <article id="citation">
+        <P>
+            "La vie a beaucoup plus d’imagination que nous”.
+        </P>
+    </article>
+    
+        <button id="button" onclick="citation()">
+            Afficher la citation
+        </button>
+   
+
+<script src="script01.js"></script>
+</body>
+</html>
---
+++ Jour02/job01/script01.js
@@ -0,0 +1,12 @@
+const texte = document.getElementById('citation');
+const bouton = document.getElementById('button');
+
+
+function citation() {
+    bouton.addEventListener('click', texte);
+    console.log(texte.innerText);
+    
+}
+
+
+ 
